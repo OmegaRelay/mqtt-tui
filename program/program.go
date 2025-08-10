c47f430f9489f4303bb9d87c9e4c9032594e9c29
Theis <theismejnertsen@gmail.com>
2025-08-10 19:02:34 +0200

    add checks an panics to program package

--- program/program.go
+++ program/program.go
@@ -13,9 +13,15 @@ func SetProgram(program *tea.Program) {
 }
 
 func Program() *tea.Program {
+	if gProgram == nil {
+		panic("get program has been called before setting program")
+	}
 	return gProgram
 }
 
 func SendErrorMsg(err error) {
+	if gProgram == nil {
+		panic("program is used before being set")
+	}
 	gProgram.Send(ErrorMsg{Err: err})
 }
